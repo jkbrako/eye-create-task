92e7b424d319ffb40e7262ea4c9c8ec5c1453d78
Jamin Brako <jamin.brako@gmail.com>
2019-03-13 10:51:01 -0400

    added new pages

--- color.js
+++ color.js
@@ -1,115 +1,173 @@
-  function startQuiz(){
-    var textInput = "";
-    document.getElementById('start').onclick= "show"
+var response = document.getElementById('textInput').innerHTML;
 
-    if(document.getElementById('start').onclick) {
-      colorOne();
-    }
+  function startQuiz(){
+    document.getElementById('start').onclick = response;
   }
+  var textInput = "";
   var results = [];
+
+
   function colorOne() {
     var textInput = "";
-    var response = document.getElementById('textInput').innerHTML;
-    if (response > 0) {
+    if (response > 0 || response < 0) {
       colorTwo();
-    } else {
+    }  else {
       textInput = "";
+    }
+      if (response = 74) {
+        console.log("CORRECT");
+        results.push = ["74"]
+      } else {
+        console.log ("WRONG");
     }
   }
 
   function colorTwo() {
-    var textInput = "";
-
+     response = textInput;
+    if (response > 0 || response < 0) {
+      colorThree();
+    } else {
+    textInput = "";
   }
+      if (response = 12) {
+        console.log("CORRECT");
+        results.push = ["12"]
+      } else {
+        console.log ("WRONG");
+      }
+    }
+
 
   function colorThree() {
     var textInput = "";
     var response = document.getElementById('textInput').innerHTML;
-    if (response > 0) {
-      colorTwo();
+    if (response > 0 || response < 0) {
+      colorFour();
     } else {
-      textInput = "";
-    }
+    textInput = "";
   }
+      if (response = 29) {
+        console.log("CORRECT");
+        results.push = ["29"]
+      } else {
+        console.log ("WRONG");
+      }
+
+}
 
   function colorFour() {
     var textInput = "";
     var response = document.getElementById('textInput').innerHTML;
-    if (response > 0) {
-      colorTwo();
+    if (response > 0 || response < 0) {
+      colorFive();
     } else {
-      textInput = "";
-    }
+    textInput = "";
   }
+      if (response = 2) {
+        console.log("CORRECT");
+        results.push = ["2"];
+      } else {
+        console.log ("WRONG");
+      }
+}
 
   function colorFive() {
     var textInput = "";
     var response = document.getElementById('textInput').innerHTML;
-    if (response > 0) {
-      colorTwo();
+    if (response > 0 || response < 0) {
+      colorSix();
     } else {
-      textInput = "";
-    }
+    textInput = "";
   }
+      if (response = 8) {
+        console.log("CORRECT");
+        results.push = ["8"];
+      } else {
+        console.log ("WRONG");
+      }
+}
 
   function colorSix() {
     var textInput = "";
     var response = document.getElementById('textInput').innerHTML;
-    if (response > 0) {
-      colorTwo();
+    if (response > 0 || response < 0) {
+      colorSeven();
     } else {
-      textInput = "";
-    }
+    textInput = "";
   }
+      if (response = 7) {
+        console.log("CORRECT");
+        results.push = ["7"]
+      } else {
+        console.log ("WRONG");
+      }
+}
 
   function colorSeven() {
     var textInput = "";
     var response = document.getElementById('textInput').innerHTML;
-    if (response > 0) {
-      colorTwo();
+    if (response > 0 || response < 0) {
+      colorEight();
     } else {
-      textInput = "";
-    }
+    textInput = "";
   }
+      if (response = 8) {
+        console.log("CORRECT");
+        results.push = ["8"]
+      } else {
+        console.log ("WRONG");
+      }
+}
 
   function colorEight() {
     var textInput = "";
     var response = document.getElementById('textInput').innerHTML;
-    if (response > 0) {
-      colorTwo();
+    if (response > 0 || response < 0) {
+      colorNine();
     } else {
       textInput = "";
     }
-  }
+      if (response = 26) {
+        console.log("CORRECT");
+        results.push = ["26"];
+      } else {
+        console.log ("WRONG");
+    }
+}
 
   function colorNine() {
     var textInput = "";
     var response = document.getElementById('textInput').innerHTML;
-    if (response > 0) {
-      colorTwo();
+    if (response > 0 || response < 0) {
+      colorTen();
     } else {
       textInput = "";
     }
+      if (response = 6) {
+        console.log("CORRECT");
+        results.push = ["6"];
+      } else {
+        console.log ("WRONG");
+      }
   }
 
   function colorTen() {
     var textInput = "";
     var response = document.getElementById('textInput').innerHTML;
-    if (response > 0) {
-        colorTwo();
+    if (response > 0 || response < 0) {
+        result();
+      } else {
+        textInput = "";
+      }
         if (response = 15) {
           console.log("CORRECT");
-          results = []
+          results.push = ["15"];
         } else {
           console.log ("WRONG");
         }
-    } else {
-      textInput = "";
-    }
   }
 
   function result() {
     // show image based how many answers are correct
     //
     }
-  }
