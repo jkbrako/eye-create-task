ac4ec30d8ef68e81be9bd6725e5d92a51ff21517
Jamin Brako <jamin.brako@gmail.com>
2019-04-05 22:55:31 -0400

    sorry progress

--- color.js
+++ color.js
@@ -1,60 +1,75 @@
+let count = 0;
+
 function result() {
     // show image based how many answers are correct
   let scores = [ ]
-  if (document.getElementById("1").innerHTML == 74) {
+  console.log()
+  if (document.getElementById("1").value == 74) {
           console.log ("CORRECT");
+          console.log("ok")
           scores.push("74");
           document.getElementById("results").value = scores;
+          count++;
       }
-    if (document.getElementById("2").innerHTML == 42) {
+    if (document.getElementById("2").value == 42) {
       console.log("CORRECT");
-      scores.push("12");
+      scores.push("42");
       document.getElementById("results").value = scores;
+      count++;
     }
-    if (document.getElementById("3").innerHTML == 29) {
+    if (document.getElementById("3").value == 29) {
           console.log ("CORRECT");
-          scores.push("74");
+          scores.push("29");
           document.getElementById("results").value = scores;
+      count++;
       }
     if (document.getElementById("4").innerHTML == 5) {
       console.log("CORRECT");
-      scores.push("12");
+      scores.push("5");
       document.getElementById("results").value = scores;
+      count++;
 
     }
-    if (document.getElementById("5").innerHTML == 8) {
-          console.log ("CORRECT");
-          scores.push("74");
-          document.getElementById("results").value = scores;
-
-      }
-    if (document.getElementById("6").innerHTML == 7) {
-      console.log("CORRECT");
-      scores.push("12");
-      document.getElementById("results").value = scores;
-    }
-    if (document.getElementById("7").innerHTML == 8) {
+    if (document.getElementById("5").value == 8) {
           console.log ("CORRECT");
           scores.push("8");
           document.getElementById("results").value = scores;
+      count++;
+
+      }
+    if (document.getElementById("6").value == 7) {
+      console.log("CORRECT");
+      scores.push("12");
+      document.getElementById("results").value = scores;
+      count++;
+    }
+    if (document.getElementById("7").value == 8) {
+          console.log ("CORRECT");
+          scores.push("8");
+          document.getElementById("results").value = scores;
+      count++;
 
       }
     if (document.getElementById("8").value == 26) {
       console.log("CORRECT");
       scores.push("26");
       document.getElementById("results").value = scores;
+      count++;
 
     }
     if (document.getElementById("9").value == 6) {
           console.log ("CORRECT");
           scores.push("6");
           document.getElementById("results").value = scores;
+      count++;
       }
     if (document.getElementById("10").value == 15) {
       console.log("CORRECT");
       scores.push("15");
       document.getElementById("results").value = scores;
+      count++;
     }
+
   if (scores.length == 0) {
     console.log("FAIL");
   }
@@ -70,7 +85,7 @@ function result() {
     document.getElementById("blind-pics").src = "blind-pics/FG-bad.jfif";
     document.getElementById("blind-pics").style.display = "block";
   }
-
+  
   document.getElementById("end").style.color = "white";
   document.getElementById("results").style.color = "grey";
   document.getElementById("results").innerHTML = 100-(((scores.length)/10)*100) + "%";
