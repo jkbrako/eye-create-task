4441d01b413c0164911a0ada3003348d5f6d9f8f
Jamin Brako <jamin.brako@gmail.com>
2019-04-07 23:08:10 -0400

    done :)

--- color.js
+++ color.js
@@ -89,3 +89,29 @@ function reset(){
   document.getElementById("9").value = "";
   document.getElementById("10").value = "";
 }
+
+// UNCONFIRMED
+
+function randomizeItems(items)
+{
+  var cached = items.slice(0), temp, i = cached.length, rand;
+  while(--i)
+  {
+    rand = Math.floor(i * Math.random());
+    temp = cached[rand];
+    cached[rand] = cached[i];
+    cached[i] = temp;
+  }
+  return cached;
+}
+function randomizeList() {
+  var list =document.getElementById("colorPics");
+  var nodes = list.children, i=0;
+  nodes = Arrat.prototype.slice.call(nodes);
+  nodes = randomizeItems(nodes);
+  while(i < nodes.length){
+    list.appendChild(nodes[i]);
+    ++i;
+  }
+  list.style.display="block";
+}
